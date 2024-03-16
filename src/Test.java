d755fb4398f6a891e1df6f592c1458fcdb91a06c
Voronkov2004 <144661971+Voronkov2004@users.noreply.github.com>
2024-03-16 21:21:51 +0200

    Add files via upload

---
+++ src/Test.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class Test {
+    public static void main(String[] args) {
+        Human anton = new Human("Anton");
+        Bot bot = new Bot();
+        ArrayList<ArrayList<String>> map = Map.createMap();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hello there Traveller!");
+        System.out.println("Would you like to play?: Y(yes)/N(no)");
+        String answer = input.nextLine();
+        if (answer.toLowerCase().equals("y") || answer.toLowerCase().equals("yes")){
+            System.out.println("\n" + "Goodluck then!" + "\n");
+            monopoly.play(anton, bot, map);
+        }
+        else{
+            System.out.println("Good luck in your journey!");
+        }
+    }
+}
